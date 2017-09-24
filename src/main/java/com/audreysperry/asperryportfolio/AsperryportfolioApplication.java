e8cb9c770eb18d108d188de73ac380eea926c82a
Audrey Sperry <audrey.sperry@gmail.com>
2017-09-24 13:55:29 -0400

    Initial commit

---
+++ src/main/java/com/audreysperry/asperryportfolio/AsperryportfolioApplication.java
@@ -0,0 +1,12 @@
+package com.audreysperry.asperryportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsperryportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsperryportfolioApplication.class, args);
+	}
+}
---
+++ src/main/java/com/audreysperry/asperryportfolio/controllers/PortfolioController.java
@@ -0,0 +1,13 @@
+package com.audreysperry.asperryportfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PortfolioController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
